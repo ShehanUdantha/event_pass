4200493fb6bad8254f56b96a7efb002d56e37410
ShehanUdantha <udantha.baddekumbura@app360.lk>
2024-04-06 22:42:39 +0530

    fixed chunkSizeWarningLimit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), splitVendorChunkPlugin()],
   define: {
     "process.env": {},
-  },
-  build: {
-    chunkSizeWarningLimit: 1700,
-    outDir: "build",
-    minify: "esbuild",
-    manifest: true,
-    sourcemap: false,
-    reportCompressedSize: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          const HugeLibraries = ["@mui", "xlsx", "xlsx-js-style", "jodit-react", "exceljs"]; // modify as required based on libraries in use
-          if (HugeLibraries.some((libName) => id.includes(`node_modules/${libName}`))) {
-            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-          }
-        },
-      },
-    },
   },
 });
